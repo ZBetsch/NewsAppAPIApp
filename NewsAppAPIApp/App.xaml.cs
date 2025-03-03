720b5a53c80ee39700504299ac6f04d93cdfac7a
Zach Betsch <betschzt@mail.uc.edu>
2025-03-03 09:17:33 -0500

    Add project files.

---
+++ NewsAppAPIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NewsAppAPIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

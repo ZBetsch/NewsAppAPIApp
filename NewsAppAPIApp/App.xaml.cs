c9d0ccfdaa53b842049ee002165cb4239ec1a804
Zach Betsch <betschzt@mail.uc.edu>
2025-03-03 09:47:49 -0500

    Homepage created. XAML added

--- NewsAppAPIApp/App.xaml.cs
+++ NewsAppAPIApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace NewsAppAPIApp
+﻿using NewsAppAPIApp.Pages;
+
+namespace NewsAppAPIApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NewsHomepage();
         }
     }
 }
